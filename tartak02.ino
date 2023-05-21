dda38c7d17bb7ef1f8c40de8208d1d0102abbc50
lukkulis <lukkulis@gmail.com>
2023-05-21 19:24:26 +0200

    Update tartak02.ino

--- tartak02.ino
+++ tartak02.ino
@@ -1,4 +1,4 @@
-#include <LiquidCrystal_I2C.h>
+//  #include <LiquidCrystal_I2C.h>
 #include "DFRobot_GP8403.h"
 #include "EmonLib.h"
 #include <WiFi.h>
@@ -8,8 +8,10 @@ const char* password = "Polinezyjska7/6";
 
 EnergyMonitor emon1;
 EnergyMonitor emon2;
+EnergyMonitor emon3;
+EnergyMonitor emon4;
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+//  LiquidCrystal_I2C lcd(0x27, 16, 2);
 DFRobot_GP8403 dac(&Wire,0x5F);
 
 int VOLTS;
@@ -41,17 +43,17 @@ void initWiFi() {
 void setup()
 {
 
-//  initWiFi();
-//  Serial.print("RRSI: ");
-//  Serial.println(WiFi.RSSI());
+  initWiFi();
 
-  lcd.init();
-  lcd.backlight();
+//  lcd.init();
+//  lcd.backlight();
 
   Serial.begin(115200);
 
   emon1.current(36, 7.1);             // Current: input pin, calibration.
   emon2.current(39, 7.1);             // Current: input pin, calibration.
+  emon3.current(34, 7.1);             // Current: input pin, calibration.
+  emon4.current(35, 7.1);             // Current: input pin, calibration.
 
   while(dac.begin()!=0){
   Serial.println("init error");
@@ -79,9 +81,9 @@ void loop()
   currentState5 = digitalRead(BUTTON_PIN5);
   currentState14 = digitalRead(BUTTON_PIN14);
 
-//  Serial.print("RRSI: ");
-//  Serial.println(WiFi.RSSI());
-//  Serial.println(WiFi.localIP());
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
+  Serial.println(WiFi.localIP());
 
   if(currentState5 == LOW) {
     Serial.println("PRESSED 5");
@@ -115,30 +117,39 @@ void loop()
   
 double Irms1 = emon1.calcIrms(3333);  // Calculate Irms only
 double Irms2 = emon2.calcIrms(3333);  // Calculate Irms only
+double Irms3 = emon3.calcIrms(3333);  // Calculate Irms only
+double Irms4 = emon4.calcIrms(3333);  // Calculate Irms only
 //  Serial.print("WATTS1: ");
 //  Serial.print(Irms1*230.0);         // Apparent power
-//  Serial.print(" AMPS1: ");
-//  Serial.println(Irms1);          // Irms
+
+  Serial.print(" AMPS1: ");
+  Serial.println(Irms1);          // Irms
   
 //  Serial.print("WATTS2: ");
 //  Serial.print(Irms2*230.0);         // Apparent power
-//  Serial.print(" AMPS2: ");
-//  Serial.println(Irms2);          // Irms
+  Serial.print(" AMPS2: ");
+  Serial.println(Irms2);          // Irms
 
-  lcd.setCursor(0, 0);
-  lcd.print("1");
-  lcd.setCursor(2, 0);
-  lcd.print(Irms1);
-  lcd.setCursor(8, 0);
-  lcd.print("2");
-  lcd.setCursor(10, 0);
-  lcd.print(Irms2);
-  lcd.setCursor(0, 1);
-  lcd.print("POSUW");
-  lcd.setCursor(8, 1);
-  lcd.print(VOLTS/100);
-  lcd.setCursor(11, 1);
-  lcd.print("%");
+    Serial.print(" AMPS3: ");
+  Serial.println(Irms3);          // Irms
+
+  Serial.print(" AMPS4: ");
+  Serial.println(Irms4);          // Irms
+
+//  lcd.setCursor(0, 0);
+//  lcd.print("1");
+//  lcd.setCursor(2, 0);
+//  lcd.print(Irms1);
+//  lcd.setCursor(8, 0);
+//  lcd.print("2");
+//  lcd.setCursor(10, 0);
+//  lcd.print(Irms2);
+//  lcd.setCursor(0, 1);
+//  lcd.print("POSUW");
+//  lcd.setCursor(8, 1);
+//  lcd.print(VOLTS/100);
+//  lcd.setCursor(11, 1);
+//  lcd.print("%");
 
 
   // save the last state
